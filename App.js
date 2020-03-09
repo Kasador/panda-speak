8059ad3eaaa4eeeb86f20be9ab34d91cddf4c00f
Kasador <huntershaw23@gmail.com>
2020-03-08 22:53:53 -0600

    UPDATE: Sign up/login page styles done.

--- App.js
+++ App.js
@@ -1,15 +1,42 @@
 import React from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
-import logo from './assets/icon.png' 
+import { Button } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import bottomGraphic from './assets/bottomPanda.png' 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Image source={logo} />
+      <View style={styles.mainContent}>
+        <Button
+          style={{margin: 15}}
+          titleStyle={styles.buttonTitle}
+          title="Sign Up"
+          type='outline'
+          onPress={()=> {
+            _test();
+          }}
+        />
+        <Button
+          style={{margin: 15}}
+          title="Login"
+          titleStyle={styles.buttonTitle}
+          type='solid'
+          onPress={()=> {
+            _test();
+          }}
+        />
+      </View>
+      <View style={styles.bottomImage}>
+        <Image source={bottomGraphic} />
+      </View>
     </View>
   );
 }
+
+const _test = (() => {
+  alert('test');
+});
 
 const styles = StyleSheet.create({
   container: {
@@ -18,5 +45,24 @@ const styles = StyleSheet.create({
     color: '#222',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  mainContent: {
+    flex: 10,
+    backgroundColor: '#ffffed',
+    color: '#222',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  bottomImage: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: 30
+  },
+  buttonTitle: {
+    fontSize: 35, 
+    width: '80%', 
+    textTransform: 'uppercase', 
+    fontFamily: 'AlNile-Bold',
   },
 });
